1455575fc9129f1a914ae07cbe9dc74226b6d43b
Andrey Falaleev <andrew.falaleev@gmail.com>
2024-06-20 21:30:55 +0100

    proxy-cli: add command to list operator accounts

--- proxy_client/__main__.py
+++ proxy_client/__main__.py
@@ -4,12 +4,14 @@ import sys
 
 from common.cmd_client.cmd_executor import BaseCmdExecutor
 from common.config.config import Config
-from .operator_balance import OpBalanceHandler
+from .operator_info_cmd import OpInfoHandler
+from .operator_balance_cmd import OpBalanceHandler
 
 
 class CmdExecutor(BaseCmdExecutor):
     def __init__(self, cfg: Config) -> None:
         super().__init__(cfg, description="Client command line utility for NeonProxy.")
+        self._handler_type_list.append(OpInfoHandler)
         self._handler_type_list.append(OpBalanceHandler)
 
 
