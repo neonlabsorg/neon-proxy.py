92297c463a4bf95b12221fd9d768a8949b9ddcea
Andrey Falaleev <andrew.falaleev@gmail.com>
2024-08-03 15:59:44 +0100

    indexer: remove tx statistic

--- indexer/stat/api.py
+++ indexer/stat/api.py
@@ -5,14 +5,6 @@ from common.utils.pydantic import BaseModel
 STATISTIC_ENDPOINT = "/api/v1/statistic/"
 
 
-class NeonTxStat(BaseModel):
-    tx_type: str
-    completed_neon_tx_cnt: int = 0
-    canceled_neon_tx_cnt: int = 0
-    sol_tx_cnt: int = 0
-    sol_expense: int = 0
-
-
 class NeonBlockStat(BaseModel):
     start_block: int
     parsed_block: int
