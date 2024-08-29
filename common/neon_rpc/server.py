f8d50d853130e721bd88374c5fc0b2ba79a4dd31
Semen Medvedev <sm@neonlabs.org>
2024-08-29 17:44:52 +0700

    NDEV-3050 Fix build

--- common/neon_rpc/server.py
+++ common/neon_rpc/server.py
@@ -67,7 +67,7 @@ class _Server:
             COMMITMENT="recent",
             EVM_LOADER=str(NeonProg.ID),
             NEON_DB_CLICKHOUSE_URLS=";".join(self._cfg.ch_dsn_list),
-            TRACER_DB_TYPE="clickhouse" if len(self._cfg.ch_dsn_list) > 0 else "none"
+            TRACER_DB_TYPE="clickhouse" if len(self._cfg.ch_dsn_list) > 0 else "none",
             SOLANA_KEY_FOR_CONFIG=self._cfg.sol_key_for_evm_cfg.to_string(),
             SOLANA_TEST_ACCOUNTS_INDEX_MEMORY_LIMIT_MB="value",  # This needs to be set in order to disable disk
             # storage for AccountsDb when running Solana Bank Emulator
