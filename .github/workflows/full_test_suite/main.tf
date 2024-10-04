c91b51b912e1b01276227b3809fa09f234e1d833
Andrey Falaleev <andrew.falaleev@gmail.com>
2024-10-04 10:15:04 +0100

    chore: CI refactoring

--- .github/workflows/full_test_suite/main.tf
+++ .github/workflows/full_test_suite/main.tf
@@ -1,6 +1,6 @@
 
 resource "hcloud_server" "proxy" {
-  name        = "proxy-${var.run_number}-${var.branch}"
+  name        = "proxy-${var.run_number}-${var.proxy_image_tag}"
   image       = data.hcloud_image.ci-image.id
   server_type = var.server_type
   location    = var.location
@@ -69,7 +69,7 @@ resource "hcloud_server" "proxy" {
 }
 
 resource "hcloud_server" "solana" {
-  name        = "solana-${var.run_number}-${var.branch}"
+  name        = "solana-${var.run_number}-${replace(var.neon_evm_commit, "_", "-")}"
   image       = data.hcloud_image.ci-image.id
   server_type = var.server_type
   location    = var.location
