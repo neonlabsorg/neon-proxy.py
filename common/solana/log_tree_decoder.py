d71b4fd5cb5791f0aa7ec18a98a3e4e1713562a8
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2024-10-24 22:40:04 +0400

    msg.decode() check

--- common/solana/log_tree_decoder.py
+++ common/solana/log_tree_decoder.py
@@ -122,7 +122,7 @@ class SolTxLogTreeDecoder:
             if msg.startswith(cls._prog_log):
                 pass
             elif msg.startswith(cls._prog_data):
-                decoded_msg = base64.b64decode(msg)
+                decoded_msg = msg.decode("utf-8")
                 log.log_list.append(decoded_msg)
             elif cls._decode_invoke(log, msg, ctx):
                 continue
