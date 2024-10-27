8296f6b7de0c422e0b79994c1577bf067cd5fcb2
Andrey Falaleev <andrew.falaleev@gmail.com>
2024-10-27 16:09:57 +0000

    chore: fix deepcopy objects

--- indexer/base/objects.py
+++ indexer/base/objects.py
@@ -180,8 +180,9 @@ class NeonIndexedHolderInfo(BaseNeonIndexedObjInfo):
         self._data = data
 
     def __deepcopy__(self, memo: dict) -> Self:
-        memo[id(self)] = copy.copy(self)
-        return self
+        new_self = copy.copy(self)
+        memo[id(self)] = new_self
+        return new_self
 
     @classmethod
     def from_raw(cls, key: Key) -> Self:
@@ -272,8 +273,9 @@ class _NeonTxEventDraft:
     neon_tx_log_idx: int | None = None
 
     def __deepcopy__(self, memo: dict) -> Self:
-        memo[id(self)] = copy.copy(self)
-        return self
+        new_self = copy.copy(self)
+        memo[id(self)] = new_self
+        return new_self
 
     @classmethod
     def from_raw(cls, src: NeonTxEventModel) -> Self:
@@ -418,7 +420,7 @@ class NeonIndexedTxInfo(BaseNeonIndexedObjInfo):
         new_self = copy.copy(self)
         new_self._is_cloned = True
         memo[id(self)] = new_self
-        return self
+        return new_self
 
     def _complete_clone(self) -> None:
         if not self._is_cloned:
