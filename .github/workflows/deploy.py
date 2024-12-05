782303c62fd3337f8ff22679122e2a4272b4bbe3
sferatime <sferatime@naver.com>
2024-12-05 16:36:26 +0700

    Debug

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -250,6 +250,7 @@ def terraform_build_infrastructure(proxy_tag, evm_tag, faucet_tag, run_number):
             if stderr:
                 file.write(stderr)
         print("stderr: ", stderr)
+        print("stdout: ", stderr)
         if return_code == 0:
             break
         elif return_code != 0:
