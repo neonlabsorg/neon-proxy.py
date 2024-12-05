2566c72a1d04f32a809febec3aa156640cd1fa56
sferatime <sferatime@naver.com>
2024-12-05 16:03:42 +0700

    Added instance retry cycle

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -234,17 +234,26 @@ def terraform_build_infrastructure(proxy_tag, evm_tag, faucet_tag, run_number):
     return_code, stdout, stderr = terraform.init(backend_config=backend_config)
     if return_code != 0:
         print("Terraform init failed:", stderr)
-    return_code, stdout, stderr = terraform.apply(skip_plan=True, capture_output=True)
-    click.echo(f"stdout: {stdout}")
-    with open(f"terraform.log", "w") as file:
-        if stdout:
-            file.write(stdout)
-        if stderr:
-            file.write(stderr)
-    if return_code != 0:
-        print("Terraform apply failed:", stderr)
-        print("Terraform infrastructure is not built correctly")
-        sys.exit(1)
+
+    instance_types = ["cpx51", "cx52"]
+    locations = ["fsn1", "nbg1"]
+    instances = [{"server_type": i, "location": j} for i in instance_types for j in locations]
+
+    for i in instances:
+        return_code, stdout, stderr = terraform.apply(skip_plan=True, capture_output=True, var={'server_type':i["server_type"], 'location':i["location"]})
+        click.echo(f"stdout: {stdout}")
+        with open(f"terraform.log", "w") as file:
+            if stdout:
+                file.write(stdout)
+            if stderr:
+                file.write(stderr)
+        if return_code == 0:
+            break
+        elif return_code != 0:
+            if stderr != "error during placement (resource_unavailable)":
+                print("Terraform apply failed:", stderr)
+                print("Terraform infrastructure is not built correctly")
+                sys.exit(1)
     output = terraform.output(json=True)
     click.echo(f"output: {output}")
     proxy_ip = output["proxy_ip"]["value"]
