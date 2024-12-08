413fe062f484e39a14912b875a2cffbcdb727edd
Andrey Falaleev <andrew.falaleev@gmail.com>
2024-12-08 15:45:03 +0000

    set-version: v1.17.0-dev

--- common/neon/neon_program.py
+++ common/neon/neon_program.py
@@ -25,12 +25,14 @@ class NeonEvmProtocol(IntEnum):
     v1013 = 1013  # 1.13 -> 1.013
     v1014 = 1014  # 1.14 -> 1.014
     v1015 = 1015  # 1.15 -> 1.015
+    v1017 = 1017  # 1.17 -> 1.017
 
 
 # fmt: off
 SUPPORTED_VERSION_SET = frozenset(
     (
         NeonEvmProtocol.v1015,
+        NeonEvmProtocol.v1017,
     )
 )
 # fmt: on
