a84bcb3ca86efb0bbaa4f8d4465c7c6ab51e65e8
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-01-08 18:55:49 +0000

    ci: add more datacenters

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -237,7 +237,7 @@ def terraform_build_infrastructure(proxy_tag, evm_tag, faucet_tag, run_number):
         print("Terraform init failed:", stderr)
 
     instance_types = ["cpx51", "cx52"]
-    locations = ["nbg1", "fsn1"]
+    locations = ["nbg1", "fsn1", "hel1", "ash", "hil", "sin"]
     instances = [{"server_type": i, "location": j} for i in instance_types for j in locations]
 
     retry_amount = 10
