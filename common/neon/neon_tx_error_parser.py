51e7853fa1b576e11592898216eed5ad8c6df1be
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2025-01-14 12:23:31 +0400

    get_nonce_error fix

--- common/neon/neon_tx_error_parser.py
+++ common/neon/neon_tx_error_parser.py
@@ -42,8 +42,8 @@ class NeonTxErrorParser(SolTxErrorParser):
         for log_rec in log_list:
             if log_rec.code == NeonTxErrorLogInfo.ErrorCode.InvalidTransactionNonce:
                 address = log_rec.data[0:19]
-                state_tx_cnt = int.from_bytes(data[20:27])
-                tx_nonce = int.from_bytes(data[28:35])
+                state_tx_cnt = int.from_bytes(log_rec.data[20:27])
+                tx_nonce = int.from_bytes(log_rec.data[28:35])
                 return int(state_tx_cnt), int(tx_nonce)
         return None
 
