0c5f74440e5db6ebe6e518c5499569038188e4d6
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2025-01-14 12:56:47 +0400

    import fix

--- tests/solana/test_cb_exceeded.py
+++ tests/solana/test_cb_exceeded.py
@@ -5,7 +5,6 @@ from common.solana.pubkey import SolPubKey
 from common.solana.signature import SolTxSig
 from common.solana.transaction_meta import SolRpcTxSlotInfo
 from common.solana_rpc.transaction_error_parser import SolTxErrorParser
-from common.solana_rpc.transaction_list_sender import SolTxListSender
 from common.solana_rpc.neon_sol_tx_list_sender import NeonSolTxListSender
 from common.solana_rpc.transaction_list_sender_stat import SolTxStatClient, SolTxDoneData, SolTxFailData
 from common.utils.cached import cached_property
