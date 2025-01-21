bdb15d6a768ebbbcf3a6482a3b9ad4cf6a71dd07
AnastasiyaTarasova <tanastasiiaa@gmail.com>
2025-01-22 03:57:42 +0400

    strategy_base.py import fix

--- proxy/executor/strategy_base.py
+++ proxy/executor/strategy_base.py
@@ -9,7 +9,6 @@ from typing_extensions import Self
 
 from common.neon.evm_log_decoder import NeonEvmLogDecoder
 from common.neon.neon_program import NeonIxMode, NeonProg
-from common.solana_rpc.ws_client import SolWatchTxSession
 from common.neon.transaction_decoder import SolNeonTxMetaInfo, SolNeonTxIxMetaInfo
 from common.neon_rpc.api import EmulSolTxInfo
 from common.solana.cb_program import SolCbProg
