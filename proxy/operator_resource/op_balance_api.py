2ffd58b2c9fa53a12ab3be958d33ffe76c248cb0
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-02-02 20:55:25 +0000

    proxy-cli: optimizations for operator withdrawing

--- proxy/operator_resource/op_balance_api.py
+++ proxy/operator_resource/op_balance_api.py
@@ -19,8 +19,8 @@ class OpBalanceApi(OpResourceApi):
     @OpResourceApi.method(name="withdrawEarnedTokens")
     async def withdraw(self, request: OpWithdrawTokenRequest) -> OpWithdrawTokenResp:
         with logging_context(**request.req_id):
-            await self._op_resource_mng.withdraw(request.chain_list)
-            return OpWithdrawTokenResp(total_amount_dict=dict())
+            result = await self._op_resource_mng.withdraw(request.owner, request.chain_id)
+            return OpWithdrawTokenResp(result=result)
 
     @cached_property
     def _op_resource_mng(self) -> OpResourceMng:
