60acfeb173f6d54af5e46c5c563e0088a04e42db
sferatime <sferatime@naver.com>
2025-02-17 21:15:49 +0700

    Debug

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -290,7 +290,7 @@ def terraform_build_infrastructure(proxy_tag, evm_tag, faucet_tag, run_number):
         chmod a+x /tmp/proxy_init.sh &&
         sudo /tmp/proxy_init.sh"""
     )
-    print(ssh_stdin, ssh_stdout, ssh_stderr)
+    print(ssh_stdin.read().decode('ascii').strip("\n"), ssh_stderr.read().decode('ascii').strip("\n"), ssh_stdout.read().decode('ascii').strip("\n"))
 
     set_github_env(infra)
 
