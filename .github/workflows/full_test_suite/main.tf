19435c6d93d57791369c945ca61b61ea7daa3f6f
Sergei Bondar <mr.bondsv@gmail.com>
2025-02-21 15:10:27 +0400

    move_gha_k8s

--- .github/workflows/full_test_suite/main.tf
+++ .github/workflows/full_test_suite/main.tf
@@ -57,22 +57,6 @@ resource "null_resource" "proxy_provision" {
 
   }
 
-  # provisioner "remote-exec" {
-  #   inline = [
-  #     "echo '${hcloud_server.solana.network.*.ip[0]}' > /tmp/solana_host",
-  #     "chmod a+x /tmp/proxy_init.sh",
-  #     "sudo /tmp/proxy_init.sh"
-  #   ]
-
-  # connection {
-  #   type        = "ssh"
-  #   user        = "root"
-  #   host        = hcloud_server.proxy.ipv4_address
-  #   private_key = file("~/.ssh/ci-stands")
-  # }
-  
-  # }
-
 }
 
 resource "hcloud_server" "solana" {
@@ -92,8 +76,6 @@ resource "hcloud_server" "solana" {
   network {
     network_id = data.hcloud_network.ci-network.id
   }
-
-  # user_data = data.template_file.solana_init.rendered
 
   labels = {
     environment = "ci"
