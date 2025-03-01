bcfd37cc472d6949781ee292e1d0a78560c9218a
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-03-01 17:41:53 +0000

    rpc: estimate parallel txs in skd-tree

--- proxy/rpc/np_call_api.py
+++ proxy/rpc/np_call_api.py
@@ -1,17 +1,19 @@
 from __future__ import annotations
 
-from typing import ClassVar, Any
+import asyncio
+from typing import ClassVar, Any, Final, Sequence
 
 from pydantic import Field
 from typing_extensions import Self
 
 from common.ethereum.bin_str import EthBinStrField
 from common.ethereum.errors import EthWrongChainIdError, EthError
-from common.ethereum.hash import EthAddressField, EthHash32Field, EthAddress
+from common.ethereum.hash import EthAddressField, EthHash32Field
 from common.http.utils import HttpRequestCtx
 from common.jsonrpc.api import BaseJsonRpcModel
 from common.jsonrpc.errors import InvalidParamError
 from common.neon.address import NeonAddress
+from common.neon.block import NeonBlockHdrModel
 from common.neon.neon_program import NeonProg
 from common.neon.skd_tree import NeonSkdTreeAddress
 from common.neon.transaction_model import NeonTxModel, NeonTxType
@@ -97,27 +99,81 @@ class _RpcEmulatorResp(BaseJsonRpcModel):
         raise ValueError(f"Wrong input type: {type(raw).__name__}")
 
 
-_RpcNeonSkdSubTxModel = BaseEthCallModel
+class _RpcNeonSkdSubTxDraft(BaseEthCallModel):
+    childTransaction: HexUIntField | None = None
+    _NoChildTxIdx: Final[int] = 0xFFFF
+
+    def calc_child_idx(self, idx: int, tx_list_len: int) -> int:
+        if (child_idx := idx + 1) >= tx_list_len:
+            child_idx = self._NoChildTxIdx
+        return if_none(self.childTransaction, child_idx)
+
+    @classmethod
+    def calc_has_child(cls, child_idx: int) -> bool:
+        return child_idx != cls._NoChildTxIdx
+
+    def to_clean_copy(self, idx: int, parent_cnt: int, tx_list_len: int) -> _RpcNeonSkdSubTxModel:
+        param_dict = self.model_dump()
+        param_dict.update(dict(childTransaction=self.calc_child_idx(idx, tx_list_len)))
+        return _RpcNeonSkdSubTxModel(**param_dict, index=idx, parentCount=parent_cnt)
+
+
+class _RpcNeonSkdSubTxModel(_RpcNeonSkdSubTxDraft):
+    index: int
+    parentCount: int
+
+    @cached_property
+    def has_child(self) -> bool:
+        return self.calc_has_child(self.childTransaction)
+
+    @cached_property
+    def is_root_tx(self) -> bool:
+        return self.parentCount == 0
 
 
 class _RpcNeonSkdTxRequest(BaseEthGasModel):
     txType: HexUIntField = Field(default=NeonTxType.Scheduled.value, validation_alias="type")
     scheduledSolanaPayer: SolPubKeyField
-    txList: list[_RpcNeonSkdSubTxModel] = Field(default_factory=list, validation_alias="transactions")
+    draftTxList: list[_RpcNeonSkdSubTxDraft] = Field(default_factory=list, validation_alias="transactions")
+
+    _maxTxListLen: Final[int] = 24
+
+    @cached_property
+    def txList(self) -> list[_RpcNeonSkdSubTxModel]:
+        tx_list_len = len(self.draftTxList)
+        parent_cnt_list: list[int] = [0] * tx_list_len
+        for idx, tx in enumerate(self.draftTxList):
+            child_idx = tx.calc_child_idx(idx, tx_list_len)
+            if not tx.calc_has_child(child_idx):
+                pass
+            elif child_idx <= idx:
+                raise ValueError(f"childTransaction {child_idx} in {idx} should be more than {idx}")
+            elif child_idx >= tx_list_len:
+                raise ValueError(f"childTransaction {child_idx} in {idx} should be less than {tx_list_len}")
+            else:
+                parent_cnt_list[child_idx] += 1
+
+        return [tx.to_clean_copy(idx, parent_cnt_list[idx], tx_list_len) for idx, tx in enumerate(self.draftTxList)]
 
     def model_post_init(self, _ctx: Any) -> None:
         if not NeonTxType.is_scheduled_tx(self.txType):
             raise ValueError(f"type should be {NeonTxType.Scheduled.value}")
-        if self.scheduledSolanaPayer.is_empty:
+        elif self.scheduledSolanaPayer.is_empty:
             raise ValueError("scheduledSolanaPayer should be present")
-        if self.maxPriorityFeePerGas > self.maxFeePerGas:
+        elif self.maxPriorityFeePerGas > self.maxFeePerGas:
             raise ValueError("maxPriorityFeePerGas should be not greater than maxFeePerGas")
-        if not self.txList:
+        elif not self.draftTxList:
             raise ValueError("transactions should be present")
+        elif len(self.draftTxList) > self._maxTxListLen:
+            raise ValueError(f"transaction list is too long, should be less than {self._maxTxListLen}")
+
+        null_cnt = sum(map(lambda x: 1 if x.childTransaction is None else 0, self.draftTxList))
+        if null_cnt not in (0, len(self.draftTxList)):
+            raise ValueError("childTransaction should be present for all or none of the transactions")
 
     def validate_chain_id(self, chain_id: int) -> None:
         if (self.chainId or chain_id) != chain_id:
-            raise ValueError(f"chainId should equal to {chain_id}")
+            raise EthWrongChainIdError()
 
     def to_core_tx_list(self, chain_id: int) -> list[CoreApiTxModel]:
         payer = NeonAddress.from_raw(self.scheduledSolanaPayer, chain_id).eth_address
@@ -249,8 +305,7 @@ class NpCallApi(NeonProxyApi):
         if if_none(call.nonce, sender_acct.state_tx_cnt) != sender_acct.state_tx_cnt:
             raise EthError("nonce mismatch")
 
-        gas_limit_list = await self._gas_limit_calc.estimate_skd_tree(call.to_core_tx_list(chain_id))
-
+        gas_limit_list = await self._estimate_skd_tree_gas(call, chain_id, block)
         _, token_gas_price = await self._get_token_gas_price(ctx)
 
         base_index = sender_acct.state_tx_cnt + int.from_bytes(sender_addr.to_bytes()[:4], "little")
@@ -275,8 +330,46 @@ class NpCallApi(NeonProxyApi):
             gasList=list(gas_limit_list),
         )
 
-    def _get_tx_chain_id(self, ctx: HttpRequestCtx, tx: RpcEthTxRequest) -> int:
-        chain_id = self._get_chain_id(ctx)
-        if tx.chainId and tx.chainId != chain_id:
-            raise EthWrongChainIdError()
-        return chain_id
+    async def _estimate_skd_tree_gas(
+        self,
+        call: _RpcNeonSkdTxRequest,
+        chain_id: int,
+        block: NeonBlockHdrModel
+    ) -> Sequence[int]:
+        tx_list = call.txList
+        core_tx_list = call.to_core_tx_list(chain_id)
+
+        # build all branches from root txs, because they
+        core_tx_branch_list: list[list[tuple[int, CoreApiTxModel]]] = list()
+        for base_tx in tx_list:
+            if not base_tx.is_root_tx:
+                continue
+
+            # build branch from the root tx
+            tx = base_tx
+            core_tx_branch: list[tuple[int, CoreApiTxModel]] = [(tx.index, core_tx_list[tx.index])]
+            while tx.has_child:
+                tx = tx_list[tx.childTransaction]
+                core_tx_branch.append((tx.index, core_tx_list[tx.index]))
+
+            core_tx_branch_list.append(core_tx_branch)
+
+        if len(core_tx_branch_list) == 1:
+            return list(await self._gas_limit_calc.estimate_skd_tree(core_tx_list, block))
+
+        # run in parallel the gas estimation tasks for all branches
+        # fmt: off
+        estimate_task_list = [
+            self._gas_limit_calc.estimate_skd_tree(list(map(lambda x: x[1], core_tx_branch)), block)
+            for core_tx_branch in core_tx_branch_list
+        ]
+        # fmt: on
+        gas_branch_list = await asyncio.gather(*estimate_task_list)
+
+        # get the maximum gas from each branch
+        gas_list: list[int] = [0] * len(tx_list)
+        for gas_branch, core_tx_branch in zip(gas_branch_list, core_tx_branch_list):
+            for gas, core_idx_tx in zip(gas_branch, core_tx_branch):
+                idx, _ = core_idx_tx
+                gas_list[idx] = max(gas_list[idx], gas)
+        return gas_list
