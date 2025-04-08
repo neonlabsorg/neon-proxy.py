54f19f92e58500a95339250c6e1310cf247e2037
sferatime <sferatime@naver.com>
2025-04-08 17:47:43 +0700

    Debug

--- .github/workflows/full_test_suite/proxy_init.sh
+++ .github/workflows/full_test_suite/proxy_init.sh
@@ -76,10 +76,10 @@ EOF
 
 
 # Get list of services
-SERVICES=$(docker-compose -f /tmp/docker-compose-ci.yml -f proxy-docker-compose-ci.override.yml config --services | grep -vP "solana|gas_tank|neon_test_invoke_program_loader")
+SERVICES=$(docker-compose -f /docker-compose-ci.yml -f proxy-docker-compose-ci.override.yml config --services | grep -vP "solana|gas_tank|neon_test_invoke_program_loader")
 
 # Pull latest versions
-docker-compose -f /tmp/docker-compose-ci.yml -f proxy-docker-compose-ci.override.yml pull $SERVICES
+docker-compose -f /docker-compose-ci.yml -f proxy-docker-compose-ci.override.yml pull $SERVICES
 
 
 function wait_service() {
@@ -132,7 +132,7 @@ wait_service "solana" $SOLANA_URL $SOLANA_DATA $SOLANA_RESULT
 
 
 # Up all services
-docker-compose -f /tmp/docker-compose-ci.yml -f proxy-docker-compose-ci.override.yml up -d $SERVICES
+docker-compose -f /docker-compose-ci.yml -f proxy-docker-compose-ci.override.yml up -d $SERVICES
 
 
 # Check if Proxy is available
