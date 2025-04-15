3c37b3e4459afe1036db85ad0fa007ab06c7ba11
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-04-15 13:57:51 +0100

    core: remove support of v1.17.x

--- common/neon/neon_program.py
+++ common/neon/neon_program.py
@@ -23,7 +23,6 @@ class NeonEvmProtocol(IntEnum):
     Unknown = -1
     v1004 = 1004  # 1.4  -> 1.004
     v1014 = 1014  # 1.14 -> 1.014
-    v1017 = 1017  # 1.17 -> 1.017
     v1018 = 1018  # 1.18 -> 1.018
     v1019 = 1019  # 1.19 -> 1.019
     v1020 = 1020  # 1.20 -> 1.020
@@ -31,7 +30,6 @@ class NeonEvmProtocol(IntEnum):
 
 # fmt: off
 SUPPORTED_VERSION_SET = frozenset((
-    NeonEvmProtocol.v1017,
     NeonEvmProtocol.v1018,
     NeonEvmProtocol.v1019,
     NeonEvmProtocol.v1020,
