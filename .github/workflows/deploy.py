4ba08aa73aae7d52c86c24e5656de555d412c34c
sferatime <sferatime@naver.com>
2025-04-18 22:11:32 +0700

    Debug

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -14,7 +14,7 @@ import json
 import typing as tp
 import logging
 from urllib.parse import urlparse
-from paramiko import SSHClient
+from paramiko import SSHClient, AutoAddPolicy, RSAKey
 from scp import SCPClient
 try:
     import pandas as pd
@@ -230,13 +230,15 @@ def set_github_env(envs: tp.Dict, upper=True) -> None:
 def get_all_containers_logs():
     home_path = os.environ.get("HOME")
     artifact_logs = "./logs"
-    ssh_key = f"{home_path}/.ssh/ci-stands"
+    ssh_key = os.environ.get("SSH_KEY")
+    private_key_file = io.StringIO(ssh_key)
+    pkey = RSAKey.from_private_key(private_key_file)
     os.mkdir(artifact_logs)
     solana_ip = os.environ.get("SOLANA_IP")
     ssh_client = SSHClient()
-    ssh_client.load_system_host_keys()
+    ssh_client.set_missing_host_key_policy(AutoAddPolicy())
     ssh_client.connect(hostname=solana_ip, username='root',
-                       key_filename=ssh_key, timeout=120)
+                       pkey=pkey, timeout=120)
     services = ["solana", "postgres", "dbcreation", "indexer", "proxy", "faucet"]
     for service in services:
         upload_remote_logs(ssh_client, service, artifact_logs)
