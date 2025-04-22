776b227c435b32373a39abc3f49e3faca3b6683c
sferatime <59023962+sferatime@users.noreply.github.com>
2025-04-22 16:00:19 +0700

    DOPS-1620 | Deploy dynamic infrastructure on AWS (#289)
    * Test
    
    * Bump
    
    * Fix typo
    
    * Bump
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Bump
    
    * Bump
    
    * Bump
    
    * Test
    
    * Test
    
    * Test
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Bump
    
    * Bump
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Clean tf files
    
    * Debug
    
    * Set resource limits
    
    * Set resource limits
    
    * Debug
    
    * Bump
    
    * Debug
    
    * Debug
    
    * Add container logs output
    
    * Add container logs output
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Debug
    
    * Use nginx to connect proxy to solana node
    
    * Use nginx to connect proxy to solana node
    
    * Debug
    
    * Debug

--- .github/workflows/stand_init.sh
+++ .github/workflows/stand_init.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-
 # Install docker
-sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get -y install ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker.io
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod a+x /usr/local/bin/docker-compose
+sudo chmod 666 /var/run/docker.sock
 
 sudo apt-get -y install pbzip2
 
-# Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-
 cd /tmp
 
-# Set required environment variables
-export REVISION=${proxy_image_tag}
-export SOLANA_URL=http:\/\/${solana_ip}:8080
-export SOLANA_WS_URL=http:\/\/${solana_ip}:8900
-export NEON_EVM_COMMIT=${neon_evm_commit}
-export FAUCET_COMMIT=${faucet_model_commit}
-export CI_PP_SOLANA_URL=${ci_pp_solana_url}
-export DOCKERHUB_ORG_NAME=${dockerhub_org_name}
-export DEVNET_SOLANA_URL=${devnet_solana_url}
+whoami
+pwd
 
+echo "Print envs for debug"
+echo "REVISION=$REVISION"
+echo "SOLANA_URL=$SOLANA_URL"
+echo "SOLANA_WS_URL=$SOLANA_WS_URL"
+echo "NEON_EVM_COMMIT=$NEON_EVM_COMMIT"
+echo "FAUCET_COMMIT=$FAUCET_COMMIT"
+echo "CI_PP_SOLANA_URL=$CI_PP_SOLANA_URL"
+echo "DOCKERHUB_ORG_NAME=$DOCKERHUB_ORG_NAME"
+echo "DEVNET_SOLANA_URL=$DEVNET_SOLANA_URL"
 
-# Generate docker-compose override file
-cat > docker-compose-ci.override.yml <<EOF
+export REVISION=$REVISION
+export SOLANA_URL=$SOLANA_URL
+export SOLANA_WS_URL=$SOLANA_WS_URL
+export NEON_EVM_COMMIT=$NEON_EVM_COMMIT
+export FAUCET_COMMIT=$FAUCET_COMMIT
+export CI_PP_SOLANA_URL=$CI_PP_SOLANA_URL
+export DOCKERHUB_ORG_NAME=$DOCKERHUB_ORG_NAME
+export DEVNET_SOLANA_URL=$DEVNET_SOLANA_URL
+
+cat > docker-compose-ci.override.yml<<EOF
 version: "3"
 
 services:
@@ -39,9 +41,28 @@ services:
     container_name: solana
     environment:
       DEVNET_SOLANA_URL: $DEVNET_SOLANA_URL
-    healthcheck:
-      test: [ CMD-SHELL, "echo done" ]
-    entrypoint: "/usr/bin/sleep 10000"
+    ports:
+      - "8899:8899"
+      - "9900:9900"
+      - "8900:8900"
+      - "8001:8001"
+      - "8001-8009:8001-8009/udp"
+  
+  nginx:
+    image: nginx:latest
+    ports:
+      - "8080:8080"
+    expose:
+      - 8080
+    hostname: nginx
+    container_name: nginx
+    volumes:
+        - /var/log/nginx:/var/log/nginx
+        - /tmp/nginx.conf:/etc/nginx/nginx.conf
+    networks:
+      - net
+    entrypoint: >
+      /bin/sh -c "echo 'Nginx Configuration:' && cat /etc/nginx/nginx.conf && nginx -g 'daemon off;'"
 
   proxy:
     container_name: proxy
@@ -71,13 +92,14 @@ services:
     container_name: dbcreation
 EOF
 
+# wake up Solana
+docker-compose -f docker-compose-ci.yml -f docker-compose-ci.override.yml up -d nginx solana
 
 # Get list of services
 SERVICES=$(docker-compose -f docker-compose-ci.yml -f docker-compose-ci.override.yml config --services | grep -vP "solana|gas_tank|neon_test_invoke_program_loader")
 
 # Pull latest versions
 docker-compose -f docker-compose-ci.yml -f docker-compose-ci.override.yml pull $SERVICES
-
 
 function wait_service() {
   local SERVICE=$1
@@ -125,6 +147,7 @@ function wait_service() {
 # Check if Solana is available
 SOLANA_DATA='{"jsonrpc":"2.0","id":1,"method":"getHealth"}'
 SOLANA_RESULT='"ok"'
+SOLANA_URL="http://localhost:8080"
 wait_service "solana" $SOLANA_URL $SOLANA_DATA $SOLANA_RESULT
 
 
@@ -138,6 +161,3 @@ PROXY_DATA='{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",
 PROXY_RESULT='"number"'
 
 wait_service "proxy" $PROXY_URL "$PROXY_DATA" $PROXY_RESULT "show_docker_logs_if_fail"
-
-
-docker rm -f solana
