5067dd17458127931e8c8b7d24e7bd8758cf9039
Natalia Romanova <nataliaromanova@Natalias-MacBook-Air.local>
2025-07-22 15:20:01 +0100

    add comment to new logic
    add comment to new logic

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -129,6 +129,7 @@ def specify_image_tags(git_sha,
     if evm_sha_tag:
         evm_sha_tag_ = evm_sha_tag
         evm_tag_ = evm_tag
+        # add evm-triggered-* prefix to proxy tag when pipeline is triggered from evm repo (evm_sha_tag != "")
         proxy_tag = f"evm-triggered-{proxy_tag}"
     else:
         evm_sha_tag_ = ""
