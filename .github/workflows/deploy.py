0a9ec33190c9e74d4562bf23433a912baa98130b
romanova-natasha <47331640+romanova-natasha@users.noreply.github.com>
2025-07-22 20:56:32 +0100

    Ndev 3834 (#361)
    * rename proxy image with "evm-triggered-*" when run from evm repo
    
    rename proxy image with "evm-triggered-*" when run from evm repo
    
    * change: define evm tag logic
    
    change: define evm tag logic
    
    * name proxy image with prefix "evm-traggered-*" if evm_loader image with proxy branch name exists
    
    name proxy image with prefix "evm-traggered-*" if evm_loader image with proxy branch name exists
    
    * use image prefix only for triggered pipelines
    
    use image prefix only for triggered pipelines
    
    * add comment to new logic
    
    add comment to new logic
    
    * rm unused logic
    
    rm unused logic
    
    * rm useless logic
    
    rm useless logic
    
    ---------
    
    Co-authored-by: Natalia Romanova <nataliaromanova@Natalias-MacBook-Air.local>

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -129,6 +129,8 @@ def specify_image_tags(git_sha,
     if evm_sha_tag:
         evm_sha_tag_ = evm_sha_tag
         evm_tag_ = evm_tag
+        # add evm-triggered-* prefix to proxy tag when pipeline is triggered from evm repo (evm_sha_tag != "")
+        proxy_tag = f"evm-triggered-{proxy_tag}"
     else:
         evm_sha_tag_ = ""
         evm_tag_ = proxy_tag if is_image_exist("evm_loader", proxy_tag) else default_evm_tag
