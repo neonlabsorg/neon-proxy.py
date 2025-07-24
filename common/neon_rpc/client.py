689f1077cc1594d26ded9733cdfef5310220300c
Artem Yazkov <artem.yazkov@gmail.com>
2025-07-25 00:02:05 +0500

    NDEV-3821 all RPC calls use exists API from common.neon_rpc.api

--- common/neon_rpc/client.py
+++ common/neon_rpc/client.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import unittest
 import asyncio
 import itertools
 import logging
@@ -8,7 +9,7 @@ import uuid
 from typing import List, Sequence, Final, TypeVar, ClassVar, Union
 from pydantic import StrictInt, StrictStr, AliasChoices, Field, ConfigDict, Base64Bytes
 
-from .api import (
+from common.neon_rpc.api import (
     CoreApiResp,
     EvmConfigModel,
     BpfLoader2ExecModel,
@@ -41,37 +42,37 @@ from .api import (
     CoreApiRequest,
     TokenModel,
 )
-from ..jsonrpc.client import JsonRpcClient
-from ..config.config import Config
-from ..config.constants import ONE_BLOCK_SEC
-from ..ethereum import revert_message
-from ..ethereum.commit_level import EthCommit
-from ..ethereum.errors import EthError
-from ..ethereum.hash import EthAddress, EthHash32
-from ..http.client import HttpClient, HttpClientRequest
-from ..http.errors import PydanticValidationError
-from ..http.utils import HttpURL
-from ..neon.address import NeonAddress
-from ..neon.block import NeonBlockHdrModel
-from ..neon.neon_program import NeonProg
-from ..solana.account import SolAccountModel
-from ..solana.cb_program import SolCbProg
-from ..solana.errors import SolAltError
-from ..solana.hash import SolBlockHash
-from ..solana.pubkey import SolPubKey
-from ..solana.transaction import SolTx
-from ..solana_rpc.client import SolClient
-from ..stat.client_rpc import RpcStatClient, RpcClientRequest
-from ..utils.cached import cached_method
-from ..utils.format import if_none
-from ..utils.json_logger import log_msg
-from ..utils.pydantic import BaseModel, RootModel, HexUIntField
+from common.jsonrpc.client import JsonRpcClient
+from common.config.config import Config
+from common.config.constants import ONE_BLOCK_SEC
+from common.ethereum import revert_message
+from common.ethereum.commit_level import EthCommit
+from common.ethereum.errors import EthError
+from common.ethereum.hash import EthAddress, EthHash32
+from common.http.client import HttpClient, HttpClientRequest
+from common.http.errors import PydanticValidationError
+from common.http.utils import HttpURL
+from common.neon.address import NeonAddress
+from common.neon.block import NeonBlockHdrModel
+from common.neon.neon_program import NeonProg
+from common.solana.account import SolAccountModel
+from common.solana.cb_program import SolCbProg
+from common.solana.errors import SolAltError
+from common.solana.hash import SolBlockHash
+from common.solana.pubkey import SolPubKey
+from common.solana.transaction import SolTx
+from common.solana_rpc.client import SolClient
+from common.stat.client_rpc import RpcStatClient, RpcClientRequest
+from common.utils.cached import cached_method
+from common.utils.format import if_none
+from common.utils.json_logger import log_msg
+from common.utils.pydantic import BaseModel, RootModel, HexUIntField
 from proxy.stat.client import StatClient
 
 _LOG = logging.getLogger(__name__)
 _RespType = TypeVar("_RespType", bound=Union[BaseModel, RootModel])
 
-
+"""
 class CoreRpcRequest(BaseModel):
     @staticmethod
     def _gen_unique_id() -> str:
@@ -88,46 +89,6 @@ class CoreApiResponse(BaseModel):
         extra="allow",
         **_model_config,
     )
-
-class GetBuildInfoResponse(CoreApiResponse):
-    class CrateInfo(CoreApiResponse):
-        name: str
-        version: str
-    class Compiler(CoreApiResponse):
-        version: str
-    class Version(CoreApiResponse):
-        commit_id: str
-    timestamp: str
-    profile: str
-    optimization_level: str
-    crate_info: CrateInfo
-    compiler: Compiler
-    version_control: Version
-
-class GetConfigResponse(CoreApiResponse):
-    class Chain(CoreApiResponse):
-        id: int
-        name: str
-        token: str
-    class Config(CoreApiResponse):
-        NEON_ACCOUNT_SEED_VERSION: str
-        NEON_EVM_STEPS_LAST_ITERATION_MAX: str
-        NEON_EVM_STEPS_MIN: str
-        NEON_GAS_LIMIT_MULTIPLIER_NO_CHAINID: str
-        NEON_HOLDER_MSG_SIZE: str
-        NEON_PAYMENT_TO_TREASURE: str
-        NEON_STORAGE_ENTRIES_IN_CONTRACT_ACCOUNT: str
-        NEON_TREASURY_POOL_COUNT: str
-        NEON_TREASURY_POOL_SEED: str
-        NEON_TREE_ACCOUNT_DESTROY_FEE: str
-        NEON_TREE_ACCOUNT_FINISH_TRANSACTION_FEE: str
-        NEON_TREE_ACCOUNT_TIMEOUT: str
-    version: str
-    revision: str
-    status: str
-    environment: str
-    chains: List[Chain]
-    config: Config
 
 class GetBalanceRequest(CoreApiRequest):
     class Address(BaseModel):
@@ -232,7 +193,7 @@ class SimulateSolanaRequest(CoreApiRequest):
 
 class SimulateSolanaResponse(CoreApiResponse):
     pass
-
+"""
 
 class CoreRpcClient(JsonRpcClient):
     def __init__(self, cfg: Config, sol_client: SolClient, stat_client: StatClient) -> None:
@@ -281,13 +242,43 @@ class CoreRpcClient(JsonRpcClient):
 
     async def get_holder_account(self, address: SolPubKey) -> HolderAccountModel:
         try:
-            req = GetHolderRequest.from_dict({"pubkey": address.to_string()})
+            req = HolderAccountRequest.from_raw(address)
             resp = await self._get_holder(req)
-            return HolderAccountModel.from_dict(address, NeonProg.DefaultChainId, resp.to_dict())
+            return HolderAccountModel.from_addr(address,
+                                                NeonProg.DefaultChainId,
+                                                {
+                                                    "status": resp.status,
+                                                    "len": resp.size,
+                                                    "owner": resp.owner,
+                                                    "tx": resp.neon_tx_hash,
+                                                    "tx_type": resp.tx_type,
+                                                    "steps_executed": resp.evm_step_cnt
+                                                })
         except BaseException as exc:
             _LOG.error("error on reading holder account", exc_info=exc)
             return HolderAccountModel.new_empty(address)
 
+    async def get_neon_account_list(
+            self,
+            address_list: Sequence[NeonAddress],
+            block: NeonBlockHdrModel | None,
+    ) -> Sequence[NeonAccountModel]:
+        try:
+            req = NeonAccountListRequest.from_raw(address_list, self._get_slot(block))
+            resp = await self._get_balance(req)
+            return tuple([NeonAccountModel(sol_address=data.sol_address,
+                                           contract_sol_address=data.contract_sol_address,
+                                           state_tx_cnt=data.state_tx_cnt,
+                                           balance=data.balance,
+                                           status=data.status,
+                                           user_sol_address=data.user_sol_address,
+                                           neon_address=a)
+                          for a, data in zip(address_list, resp)])
+        except BaseException as exc:
+            _LOG.error("error on reading Neon account list", exc_info=exc)
+            return tuple([NeonAccountModel.new_empty(addr) for addr in address_list])
+
+    """
     async def get_neon_account_list(
             self,
             address_list: Sequence[NeonAddress],
@@ -303,6 +294,7 @@ class CoreRpcClient(JsonRpcClient):
         except BaseException as exc:
             _LOG.error("error on reading Neon account list", exc_info=exc)
             return tuple([NeonAccountModel.new_empty(addr) for addr in address_list])
+    """
 
     async def get_neon_account(self, address: NeonAddress, block: NeonBlockHdrModel | None) -> NeonAccountModel:
         acct_list = await self.get_neon_account_list([address], block)
@@ -313,18 +305,22 @@ class CoreRpcClient(JsonRpcClient):
         return acct.state_tx_cnt
 
     async def get_neon_contract(self, address: NeonAddress, block: NeonBlockHdrModel | None) -> NeonContractModel:
-        req = GetContractRequest.from_dict({"contract": address.to_address(), "slot": self._get_slot(block)})
+        req = NeonContractRequest(contract=address.eth_address, slot=self._get_slot(block))
         resp = await self._get_contract(req)
-        return NeonContractModel.from_dict(resp[0].to_dict(), address=address)
+        return NeonContractModel(neon_address=address, code=resp[0].code, solana_address=resp[0].sol_address)
 
     async def get_storage_at(self, contract: EthAddress, index: int, block: NeonBlockHdrModel | None) -> EthHash32:
-        req = GetStorageRequest.from_dict({
-            "contract": contract.to_string(),
-            "index": index,
-            "slot": self._get_slot(block)
-        })
+        req = NeonStorageAtRequest(contract=contract, index=index, slot=self._get_slot(block))
         resp = await self._get_storage_at(req)
-        return EthHash32.from_raw(bytes(resp.to_dict()))
+        #pprint.pp(["___", resp, "___"])
+        return EthHash32.from_raw(bytes(resp))
+        #req = GetStorageRequest.from_dict({
+        #    "contract": contract.to_string(),
+        #    "index": index,
+        #    "slot": self._get_slot(block)
+        #})
+        #resp = await self._get_storage_at(req)
+        #return EthHash32.from_raw(bytes(resp.to_dict()))
 
     async def get_neon_skd_tree(
         self,
@@ -332,12 +328,10 @@ class CoreRpcClient(JsonRpcClient):
         nonce: int,
         block: NeonBlockHdrModel | None = None,
     ) -> NeonSkdTreeModel:
-        req = GetTransactionTreeRequest.from_dict(
-            {"origin": {"address": payer.to_address(), "chain_id": payer.chain_id},
-             "nonce": nonce,
-             "slot": self._get_slot(block)})
-        resp = await self._get_transaction_tree(req)
-        return NeonSkdTreeModel.from_dict(resp.to_dict())
+        req = NeonSkdTreeRequest.from_raw(payer=payer, nonce=nonce, slot=self._get_slot(block))
+        return await self._get_transaction_tree(req)
+        #pprint.pp(resp.to_dict())
+        #return NeonSkdTreeModel.from_dict(resp.to_dict())
 
     async def get_earn_account(
         self,
@@ -523,28 +517,37 @@ class CoreRpcClient(JsonRpcClient):
         return tuple(resp.meta_list)
 
     @JsonRpcClient.method(name="build_info")
-    async def _get_build_info(self) -> GetBuildInfoResponse: ...
+    async def _get_build_info(self) -> CoreApiBuildModel: ...
 
     @JsonRpcClient.method(name="config")
-    async def _get_config(self) -> GetConfigResponse: ...
+    async def _get_config(self) -> EvmConfigModel: ...
+
+    #@JsonRpcClient.method(name="balance")
+    #async def _get_balance(self, req: GetBalanceRequest) -> List[GetBalanceResponse]: ...
 
     @JsonRpcClient.method(name="balance")
-    async def _get_balance(self, req: GetBalanceRequest) -> List[GetBalanceResponse]: ...
+    async def _get_balance(self, req: NeonAccountListRequest) -> Sequence[NeonAccountModel]: ...
+
+    #@JsonRpcClient.method(name="contract")
+    #async def _get_contract(self, req: GetContractRequest) -> List[GetContractResponse]: ...
 
     @JsonRpcClient.method(name="contract")
-    async def _get_contract(self, req: GetContractRequest) -> List[GetContractResponse]: ...
+    async def _get_contract(self, req: NeonContractRequest) -> Sequence[NeonContractModel]: ...
 
     @JsonRpcClient.method(name="holder")
-    async def _get_holder(self, req: GetHolderRequest) -> GetHolderResponse: ...
+    async def _get_holder(self, req: HolderAccountRequest) -> HolderAccountModel: ...
+
+    #@JsonRpcClient.method(name="get_storage_at")
+    #async def _get_storage_at(self, req: GetStorageRequest) -> GetStorageResponse: ...
 
     @JsonRpcClient.method(name="get_storage_at")
-    async def _get_storage_at(self, req: GetStorageRequest) -> GetStorageResponse: ...
+    async def _get_storage_at(self, req: NeonStorageAtRequest) -> Sequence[HexUIntField]: ...
+
+    #@JsonRpcClient.method(name="transaction_tree")
+    #async def _get_transaction_tree(self, req: GetTransactionTreeRequest) -> GetTransactionTreeResponse: ...
 
     @JsonRpcClient.method(name="transaction_tree")
-    async def _get_transaction_tree(self, req: GetTransactionTreeRequest) -> GetTransactionTreeResponse: ...
-
-    @JsonRpcClient.method(name="transaction_tree")
-    async def _get_transaction_tree(self, req: GetTransactionTreeRequest) -> GetTransactionTreeResponse: ...
+    async def _get_transaction_tree(self, req: NeonSkdTreeRequest) -> NeonSkdTreeModel: ...
 
     @JsonRpcClient.method(name="emulate")
     async def _emulate(self, req: EmulNeonCallRequest) -> EmulNeonCallResp: ...
@@ -593,3 +596,87 @@ class CoreRpcClient(JsonRpcClient):
 
 class CoreApiClient(CoreRpcClient):
     pass
+
+
+
+class TestCoreRpcClient(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        config = Config()
+        self._stat_client = StatClient(config)
+        self._sol_client = SolClient(config, self._stat_client)
+        self._rpc_client = CoreRpcClient(config,
+                                         self._sol_client,
+                                         self._stat_client).connect(host="127.0.0.1", port=9104)
+        self._api_client = CoreApiClient(config,self._sol_client, self._stat_client)
+        await self._stat_client.start()
+        await self._sol_client.start()
+
+    async def asyncTearDown(self):
+        await self._api_client.stop()
+        await self._rpc_client.stop()
+        await self._sol_client.stop()
+        await self._stat_client.stop()
+
+    @unittest.skip
+    async def test_core_api_version(self):
+        resp = await self._rpc_client.get_core_api_version()
+        pprint.pp(resp)
+
+    @unittest.skip
+    async def test_evm_cfg(self):
+        resp = await self._rpc_client.get_evm_cfg()
+        pprint.pp(resp.to_dict())
+
+    @unittest.skip
+    async def test_holder_account(self):
+        address = SolPubKey.from_string("2f372pRn7EMdw5zXg777AJT35KdjLSssFN5WF4tEwxLf")
+        resp = await self._api_client.get_holder_account(address)
+        pprint.pp(resp.to_dict())
+
+    @unittest.skip
+    async def test_neon_account_list(self):
+        address_list = [
+            NeonAddress.from_raw("0x5860522454aa6bF2c4a09D301e004cd334038Ff8", 114),
+            NeonAddress.from_raw("0x5860522454aa6bF2c4a09D301e004cd334038Ff8", 114)
+        ]
+        resp = await self._rpc_client.get_neon_account_list(address_list, NeonBlockHdrModel.default())
+        for acc in resp:
+            pprint.pp(acc.to_dict())
+        pprint.pp(NeonAccountModel.new_empty(address_list[0]).to_dict())
+        pass
+
+    @unittest.skip
+    async def test_neon_contract(self):
+        address = NeonAddress.from_raw("0x5860522454aa6bF2c4a09D301e004cd334038Ff8", 114)
+        resp = await self._rpc_client.get_neon_contract(address, NeonBlockHdrModel.default())
+        pprint.pp(resp.to_dict())
+
+    #@unittest.skip
+    async def test_storage_at(self):
+        contract = EthAddress.from_raw("0x3e44a5098621C0B7E1B76edBF8Da18252d74D360")
+        resp = await self._rpc_client.get_storage_at(contract, 1, NeonBlockHdrModel.default())
+        pprint.pp(resp)
+
+    @unittest.skip
+    async def test_neon_skd_tree(self):
+        payer = NeonAddress.from_raw("0x43d06925D5B01fe1ABFCb13DB0F3a706106A8e03", 112)
+        resp = await self._rpc_client.get_neon_skd_tree(payer, 0, NeonBlockHdrModel.default())
+        #pprint.pp(resp.to_dict())
+
+    @unittest.skip
+    async def test_simulate_solana(self):
+        req = EmulSolTxListRequest.from_dict({
+            "account_cnt_limit": 255,
+            "blockhash": "0691a8386ec771ec9914c7a0a26d67ca2bb04f22cc1a42c5d110c0d2027ed604",
+            "cu_limit": 1400000,
+            "heap_size": 262144,
+            "tx_list": ["018c4cda5240ea6c1e0e1e8f969aca87497484700b7b71cb93d2452f17a31f9c1ddadb9c4eaf92f32e03e5dc26c4b094f666e5ede1986a4a1fc96f4ac9ffcbbb0e0100030a7bf08608a676d074e5232735cbce615f166bec3e46c8bf3fdd229a5b9f99b4ab19df5a324e77a9f550a53a8a024cd268eb8df44c885175cd4e3a51558df1b82239c2350b7d1034884000b2b96e9482484cf6fc9a5cea7e92861702af2685872b46ff27d03dd553c765b1d2cb2ae16172a989eacb90b292edba8540105970f8b16e14438ed9206aa8b1d1c56f0a3334c897cb1200d09f5c04ec4bd0f4ed754cc5948363f8f2ae302c947d301c055f268285526121c9c7b8cbfa58def895f73fa4c24beca9b9e4482b5a124f178d944a64417a65bb36cab34117cbb8b6fd700da700000000000000000000000000000000000000000000000000000000000000000306466fe5211732ffecadba72c39be7bc8ce5bbc5f7126b2c439b3a400000003c00392b787d38a853d124057634c43c7133c612461d74feb17f4248155286c00691a8386ec771ec9914c7a0a26d67ca2bb04f22cc1a42c5d110c0d2027ed6040408000903042900000000000008000502c05c150008000501000004000908020005060701030493043d76000000f9020b0f849c76524184013ad9ef8080b901b6608060405234801561001057600080fd5b50610196806100206000396000f3fe608060405234801561001057600080fd5b506004361061002b5760003560e01c80639c4ae2d014610030575b600080fd5b61004361003e3660046100ab565b610045565b005b6000818351602085016000f56040516001600160a01b03821681529091507f55ea6c6b31543d8e2ec6a72f71a79c0f4b72ed0d4757172b043d8f4f4cd848489060200160405180910390a1505050565b634e487b7160e01b600052604160045260246000fd5b600080604083850312156100be57600080fd5b823567ffffffffffffffff808211156100d657600080fd5b818501915085601f8301126100ea57600080fd5b8135818111156100fc576100fc610095565b604051601f8201601f19908116603f0116810190838211818310171561012457610124610095565b8160405282815288602084870101111561013d57600080fd5b82602086016020830137600060209382018401529896909101359650505050505056fea264697066735822122029400b0d7e26fec77a532d21ada2fda50b813bcf63f041e8ba162d16e10ea56964736f6c634300080a0033820102a09d87d70c6c4704228ee49506eab0d11ddf21005978bdcdfc3b86f5007ea2c6b4a0355f4d0b7d38b972c1b8afa2d0085d4cf669f3dbe106eb33eea350dac5db2b9c"],
+            "verify": False
+        })
+        resp = await self._rpc_client._simulate_solana(req)
+        pprint.pp(resp.meta_list[0].to_dict())
+
+if __name__ == "__main__":
+    unittest.main()
+#
+#
