1d80aee14a10f05981004818f4348c1756c43a3a
Artem Yazkov <artem.yazkov@gmail.com>
2025-07-25 16:44:52 +0500

    NDEV-3821 class CoreRpcServer: take SOLANA_URL from cfg.sol_url_list

--- common/neon_rpc/server.py
+++ common/neon_rpc/server.py
@@ -125,8 +125,9 @@ class _ApiInstance(_BaseInstance):
 
 
 class _RpcInstance(_BaseInstance):
-    def __init__(self, cfg: Config, idx: int):
+    def __init__(self, cfg: Config, idx: int, solana_url: str):
         super().__init__(cfg, idx)
+        self._solana_url = solana_url
         self._run_cmd = [cfg.neon_core_api_server_bin, cfg.neon_core_api_server_libdir]
 
 
@@ -160,7 +161,8 @@ class CoreRpcServer:
 
         idx = itertools.count()
         for _ in range(cfg.neon_core_api_server_cnt):
-            self._instance_list.append(_RpcInstance(cfg, next(idx)))
+            for url in cfg.sol_url_list:
+                self._instance_list.append(_RpcInstance(cfg, next(idx), url))
 
     def start(self) -> None:
         for instance in self._instance_list:
