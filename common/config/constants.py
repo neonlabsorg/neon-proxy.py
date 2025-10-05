9aab75966890647dfd9ea6367a57b8370d2ca6fb
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-10-05 17:56:51 +0100

    core: add ONE_BLOCK_MSEC constant

--- common/config/constants.py
+++ common/config/constants.py
@@ -6,6 +6,7 @@ from ..solana.pubkey import SolPubKey
 ######################################
 # Solana general settings:
 ONE_BLOCK_SEC: Final[float] = float(os.environ.get("SOLANA_BLOCK_SEC", "0.4"))
+ONE_BLOCK_MSEC: Final[int] = int(ONE_BLOCK_SEC * 1000.0)
 MIN_FINALIZE_BLOCK: Final[int] = 32
 MIN_FINALIZE_SEC: Final[float] = ONE_BLOCK_SEC * MIN_FINALIZE_BLOCK
 SOL_PKT_SIZE: Final[int] = 1280 - 40 - 8
