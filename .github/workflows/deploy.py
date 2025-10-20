38790714fe68fce6508983c44b22badddbdd4b5a
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-10-20 14:01:11 +0100

    ci: set neon-tests image tag

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -138,18 +138,19 @@ def specify_image_tags(git_sha,
     faucet_tag = proxy_tag if is_image_exist("neon-faucet", proxy_tag) else default_faucet_tag
 
     # test_image_tag
-    if is_proxy_release:
-        neon_test_tag = proxy_tag
-        if not is_image_exist(NEON_TESTS_IMAGE, neon_test_tag):
-            raise RuntimeError(f"{NEON_TESTS_IMAGE} image with {neon_test_tag} tag isn't found")
-    elif evm_tag and is_image_exist(NEON_TESTS_IMAGE, evm_tag):
-        neon_test_tag = evm_tag
-    elif is_image_exist(NEON_TESTS_IMAGE, proxy_tag):
-        neon_test_tag = proxy_tag
-    elif proxy_pr_version_branch and is_image_exist(NEON_TESTS_IMAGE, proxy_pr_version_branch):
-        neon_test_tag = proxy_pr_version_branch
-    else:
-        neon_test_tag = "latest"
+    # if is_proxy_release:
+    #     neon_test_tag = proxy_tag
+    #     if not is_image_exist(NEON_TESTS_IMAGE, neon_test_tag):
+    #         raise RuntimeError(f"{NEON_TESTS_IMAGE} image with {neon_test_tag} tag isn't found")
+    # elif evm_tag and is_image_exist(NEON_TESTS_IMAGE, evm_tag):
+    #     neon_test_tag = evm_tag
+    # elif is_image_exist(NEON_TESTS_IMAGE, proxy_tag):
+    #     neon_test_tag = proxy_tag
+    # elif proxy_pr_version_branch and is_image_exist(NEON_TESTS_IMAGE, proxy_pr_version_branch):
+    #     neon_test_tag = proxy_pr_version_branch
+    # else:
+    #     neon_test_tag = "latest"
+    neon_test_tag = "v1.20.4"
 
     env = dict(proxy_tag=proxy_tag,
                proxy_sha_tag=proxy_sha_tag,
