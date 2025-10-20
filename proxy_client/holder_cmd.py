98a915d338d4036855c0bf0fd253e8ca50559c35
Andrey Falaleev <andrew.falaleev@gmail.com>
2025-10-20 13:31:42 +0100

    proxy-cli: fix cancel command

--- proxy_client/holder_cmd.py
+++ proxy_client/holder_cmd.py
@@ -5,7 +5,7 @@ from typing import Final
 from typing_extensions import Self
 
 from common.config.config import Config
-from common.neon.cancel_error import CancelErrorSource, NeonProxyCancelErrorCode
+from common.neon.cancel_error import CancelErrorSource, NeonProxyCancelErrorCode, CancelErrorData
 from common.neon.neon_program import NeonProg, NeonEvmIxCode, NeonBaseTxAccountSet
 from common.neon_rpc.api import HolderAccountStatus, HolderAccountModel
 from common.neon_rpc.client import CoreApiClient
@@ -274,7 +274,7 @@ class HolderHandler(BaseNPCmdHandler):
         )
         # fmt: on
 
-        data = CancelErrorSource(CancelErrorSource.NeonProxy, NeonProxyCancelErrorCode.Manual, "Unknown")
+        data = CancelErrorData(CancelErrorSource.NeonProxy, NeonProxyCancelErrorCode.Manual, "Unknown")
         cancel_ix = neon_prog.make_cancel_ix(data.to_bytes())
 
         ix_list = self._get_cb_ix_list() + [cancel_ix]
